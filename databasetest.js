52e6c9b7f56f852c39616608ca473672b6e0efe8
Parsa <parsalotfy@gmail.com>
2025-05-14 12:09:12 +0200

    get insert working

--- databasetest.js
+++ databasetest.js
@@ -1,12 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb://localhost:27017/admin';
+import express from 'express';
+import mongoose from 'mongoose';
+
+const connectionString = 'mongodb://localhost:27017/admin';
 const app = express();
 app.use(express.json());
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+// test connection to local database
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true,})
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
+
+// create schema for data
+const userSchemaTest = mongoose.Schema({
+  name: String,
+  password: String,
+});
+
+// create model for user
+const userModel = mongoose.model('User', userSchemaTest, 'Users');
+
+// insert test user to collection in local db
+async function InsertUser() {
+  await userModel.create([
+    { name: 'admin', password: 'secret' },
+  ]);
+  console.log('Sample user created');
+}
+
+// call function
+await InsertUser();
