970ab1cc0f8f505ed1d16353df768c525fdc33c7
Alagu <qak83zit@rhrk.uni-kl.de>
2025-05-20 10:07:00 +0200

    added watering job API

---
+++ jobs/WateringJob.js
@@ -0,0 +1,41 @@
+import { FarmbotStatus } from "../statusManager.js";
+import { Job } from "./Job.js"
+import { MoveTask } from "./tasks/MoveTask.js";
+import { MoveZAxisTask} from "./tasks/MoveZAxisTask.js";
+import { TogglePinTask, VACUUM_PIN, WATER_PIN } from "./tasks/TogglePinTask.js";
+import {TimedPinTask} from "./tasks/TimedPinTask.js";
+
+/*
+Steps:
+- move to watering coordinates (Move Command)
+- water(Toggle Pin Command)
+- repeat till all the given plants are finished
+*/
+
+const SEEDING_HEIGHT = -350
+const FIELD_SAFETY_HEIGHT = -300
+const SEED_BOWL_SAFETY_HEIGHT = 0;
+
+class WateringJob extends Job {
+    constructor(wateringArgs) {
+        super();
+        this.duration = wateringArgs["duration"];
+        this.position = wateringArgs["position"];
+
+        let goToSafetyHeight = new MoveZAxisTask(FarmbotStatus.FETCHING, SEED_BOWL_SAFETY_HEIGHT);
+        this.taskQueue.enqueue(goToSafetyHeight);
+
+        wateringArgs["position"]["z"] = Math.max(FIELD_SAFETY_HEIGHT,wateringArgs["position"]["z"]);
+        let goToWateringPosition = new MoveTask(FarmbotStatus.MOVING_TO_WATERING_POSITION, wateringArgs["position"]);
+        this.taskQueue.enqueue(goToWateringPosition);
+
+        let waterSeeds = new TimedPinTask(FarmbotStatus.WATERING, WATER_PIN, this.duration);
+        this.taskQueue.enqueue(waterSeeds);
+
+        let returnToFieldSafetyHeight = new MoveZAxisTask(FarmbotStatus.WATERING, FIELD_SAFETY_HEIGHT);
+        this.taskQueue.enqueue(returnToFieldSafetyHeight);
+        
+    }
+}
+
+export {WateringJob};
--- temp.js
+++ temp.js
@@ -1,6 +1,7 @@
 import {SeedingJob} from "./jobs/SeedingJob.js";
 import { StatusManager } from "./statusManager.js";
 import {getFarmbot} from './farmbotInitializer.js';
+import { WateringJob } from "./jobs/WateringJob.js";
 
 let farmbot = await getFarmbot()
 let statusManager = new StatusManager(farmbot);
@@ -17,7 +18,13 @@ function waitForFirstStatus(farmbot) {
 
 await waitForFirstStatus(farmbot);
 console.log("StatusManager Initialized");
+
 let seedingArgs = {"position": {"x": 100, "y": 100}}
 let seedingJob = new SeedingJob(seedingArgs);
 
 statusManager.startJob(seedingJob);
+
+let WateringArgs ={"position": {"x": 100, "y": 100,"z": -50}, "duration":10}
+let wateringJob = new WateringJob(WateringArgs);
+
+statusManager.startJob(wateringJob);
