dcb2df1a020117839c8076862762c0621582bb1d
Daniel Malle <dmalle@rhrk.uni-kl.de>
2025-06-10 13:21:11 +0200

    fix module import

--- backend/scheduleManager.js
+++ backend/scheduleManager.js
@@ -1,4 +1,5 @@
-import { DatabaseService, JobType } from "../databaseservice.js";
+import DatabaseService from "../databaseservice.js";
+import JobType from "../databaseservice.js";
 
 const SCHEDULE_CHECKING_INTERVAL = 900000 // 15*60*1000 = 15min
 const SCHEDULE_TOLERANCE = 15000; // 15 Second execution tolerance
