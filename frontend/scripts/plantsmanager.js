83e7ce67a7245ff39e83c8e3797805398b9dc556
Daniel Malle <dmalle@rhrk.uni-kl.de>
2025-07-10 15:09:29 +0200

    add temporary predefined plants

--- frontend/scripts/plantsmanager.js
+++ frontend/scripts/plantsmanager.js
@@ -142,6 +142,11 @@ export async function getPlants() {
     .catch(error => console.error('Error fetching plants:', error));
 }
 
-function capitalize(str) {
-  return String(str).charAt(0).toUpperCase() + str.slice(1);
-}
+export const predefinedPlants = [
+  { id:1, planttype: "lettuce", plantname: "Luna", xcoordinate: 20, ycoordinate: 20 },
+  { id:2, planttype: "lettuce", plantname: "Leafy", xcoordinate: 40, ycoordinate: 20 },
+  { id:3, planttype: "tomato", plantname: "Ruby", xcoordinate: 20, ycoordinate: 40 },
+  { id:4, planttype: "tomato", plantname: "Sunny", xcoordinate: 40, ycoordinate: 40 },
+  { id:5, planttype: "radish", plantname: "Spicy", xcoordinate: 20, ycoordinate: 60 },
+  { id:6, planttype: "radish", plantname: "Crunch", xcoordinate: 40, ycoordinate: 60 }
+];
