e8a63aa4d817a894a0c2191a4f7d43e1d4e0ff95
pmaity1995 <prasantamaity549@gmail.com>
2025-07-17 11:08:41 +0200

    Seeding_validation

--- frontend/scripts/seeding.js
+++ frontend/scripts/seeding.js
@@ -190,7 +190,7 @@ executeBtn.addEventListener('click', async () => {
       err.textContent = getTranslation('noPlantName');
       valid = false;
     } */
-      else if (isNaN(x) || isNaN(y) || x <= 0 || x >= 395 || y <= 0 || y >= 650) {
+      else if (isNaN(x) || isNaN(y) || x < 10 || x >= 395 || y < 10 || y >= 650) {
       err.textContent = getTranslation('correctValues');
       valid = false;
     } else {
@@ -270,7 +270,7 @@ modifyExecuteBtn.addEventListener('click', async () => {
     }else if (plantName=== '') {
       err.textContent = getTranslation('noPlantName');
       valid = false;
-    } else if (isNaN(x) || isNaN(y) || x <= 0 || x >= 395 || y <= 0 || y >= 650) {
+    } else if (isNaN(x) || isNaN(y) || x < 10 || x >= 395 || y < 10 || y >= 650) {
       err.textContent = getTranslation('correctValues');
       valid = false;
     } else {
